d662f75300b980b7917723589b23a729e893733a
Ami-ka <kuzhalovamir2006@gmail.com>
2025-05-31 22:10:14 +0500

    Revert "configure cors"
    This reverts commit ecbd19077b996c79cc73ae40be1a8c7c30d0b341.

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://blog-zeta-red-59.vercel.app/'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
